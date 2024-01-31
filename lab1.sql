e2dddf3255bf10b3b107e83a488b22d341e7caf0
stuckpixel0 <157515866+stuckpixel0@users.noreply.github.com>
2024-01-31 09:35:09 +0530

    Update lab1.sql

--- lab1.sql
+++ lab1.sql
@@ -1,33 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 21, 2024 at 06:54 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `lab1`
---
 CREATE DATABASE IF NOT EXISTS `lab1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `lab1`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `book`
---
 
 CREATE TABLE `book` (
   `Book_Id` int(11) NOT NULL,
@@ -36,9 +16,7 @@ CREATE TABLE `book` (
   `Pub_Year` year(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `book`
---
+
 
 INSERT INTO `book` (`Book_Id`, `Title`, `Publisher_Name`, `Pub_Year`) VALUES
 (101, 'Python', 'Yashu', '2001'),
@@ -46,23 +24,13 @@ INSERT INTO `book` (`Book_Id`, `Title`, `Publisher_Name`, `Pub_Year`) VALUES
 (103, 'html', 'Sonu', '0000'),
 (104, 'C++', 'Manya', '2005');
 
--- --------------------------------------------------------
-
---
--- Stand-in structure for view `books_avail`
--- (See below for the actual view)
---
 CREATE TABLE `books_avail` (
 `Book_Id` int(11)
 ,`Title` varchar(40)
 ,`No_of_Copies` int(11)
 );
 
--- --------------------------------------------------------
 
---
--- Table structure for table `book_authors`
---
 
 CREATE TABLE `book_authors` (
   `Book_Id` int(11) NOT NULL,
@@ -79,11 +47,6 @@ INSERT INTO `book_authors` (`Book_Id`, `Author_Name`) VALUES
 (103, 'Lavanya'),
 (104, 'Pruthvi');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `book_copies`
---
 
 CREATE TABLE `book_copies` (
   `Book_Id` int(11) NOT NULL,
@@ -91,9 +54,6 @@ CREATE TABLE `book_copies` (
   `No_of_Copies` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `book_copies`
---
 
 INSERT INTO `book_copies` (`Book_Id`, `Program_Id`, `No_of_Copies`) VALUES
 (101, 51, 20),
@@ -101,11 +61,6 @@ INSERT INTO `book_copies` (`Book_Id`, `Program_Id`, `No_of_Copies`) VALUES
 (103, 53, 10),
 (104, 54, 5);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `book_lending`
---
 
 CREATE TABLE `book_lending` (
   `Book_Id` int(11) NOT NULL,
@@ -115,10 +70,6 @@ CREATE TABLE `book_lending` (
   `Due_Date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `book_lending`
---
-
 INSERT INTO `book_lending` (`Book_Id`, `Program_Id`, `Card_No`, `Date_Out`, `Due_Date`) VALUES
 (101, 51, 1, '2017-01-01', '2017-04-03'),
 (102, 52, 1, '2017-02-02', '2017-03-15'),
@@ -127,31 +78,17 @@ INSERT INTO `book_lending` (`Book_Id`, `Program_Id`, `Card_No`, `Date_Out`, `Due
 (104, 51, 1, '2017-01-10', '2017-03-15'),
 (104, 54, 2, '2018-02-10', '2018-03-15');
 
--- --------------------------------------------------------
 
---
--- Stand-in structure for view `book_view`
--- (See below for the actual view)
---
 CREATE TABLE `book_view` (
 `PUB_YEAR` year(4)
 );
 
--- --------------------------------------------------------
 
---
--- Stand-in structure for view `book_view1`
--- (See below for the actual view)
---
 CREATE TABLE `book_view1` (
 `PUB_YEAR` year(4)
 );
 
--- --------------------------------------------------------
 
---
--- Table structure for table `library_program`
---
 
 CREATE TABLE `library_program` (
   `Program_Id` int(11) NOT NULL,
@@ -159,9 +96,7 @@ CREATE TABLE `library_program` (
   `Address` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `library_program`
---
+
 
 INSERT INTO `library_program` (`Program_Id`, `Program_Name`, `Address`) VALUES
 (51, 'CSE', 'Mysore'),
@@ -170,12 +105,7 @@ INSERT INTO `library_program` (`Program_Id`, `Program_Name`, `Address`) VALUES
 (54, 'EEE', 'Kerala'),
 (55, 'Civil', 'Mandya');
 
--- --------------------------------------------------------
 
---
--- Stand-in structure for view `p1`
--- (See below for the actual view)
---
 CREATE TABLE `p1` (
 `Book_Id` int(11)
 ,`Title` varchar(40)
@@ -183,11 +113,7 @@ CREATE TABLE `p1` (
 ,`Pub_Year` year(4)
 );
 
--- --------------------------------------------------------
 
---
--- Table structure for table `publisher`
---
 
 CREATE TABLE `publisher` (
   `Name` varchar(20) NOT NULL,
@@ -195,9 +121,7 @@ CREATE TABLE `publisher` (
   `Phone` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `publisher`
---
+
 
 INSERT INTO `publisher` (`Name`, `Address`, `Phone`) VALUES
 ('Manju', 'Bangalore', 9148644600),
@@ -206,125 +130,73 @@ INSERT INTO `publisher` (`Name`, `Address`, `Phone`) VALUES
 ('Teju', 'Manglore', 9164024258),
 ('Yashu', 'Maddur', 9148644680);
 
--- --------------------------------------------------------
 
---
--- Structure for view `books_avail`
---
 DROP TABLE IF EXISTS `books_avail`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `books_avail`  AS SELECT `b`.`Book_Id` AS `Book_Id`, `b`.`Title` AS `Title`, `c`.`No_of_Copies` AS `No_of_Copies` FROM (`book` `b` join `book_copies` `c`) WHERE `b`.`Book_Id` = `c`.`Book_Id` ;
 
--- --------------------------------------------------------
 
---
--- Structure for view `book_view`
---
 DROP TABLE IF EXISTS `book_view`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `book_view`  AS SELECT `book`.`Pub_Year` AS `PUB_YEAR` FROM `book` ;
 
--- --------------------------------------------------------
 
---
--- Structure for view `book_view1`
---
 DROP TABLE IF EXISTS `book_view1`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `book_view1`  AS SELECT `book`.`Pub_Year` AS `PUB_YEAR` FROM `book` WHERE `book`.`Pub_Year` between 2001 and 2004 ;
 
--- --------------------------------------------------------
 
---
--- Structure for view `p1`
---
 DROP TABLE IF EXISTS `p1`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `p1`  AS SELECT `book`.`Book_Id` AS `Book_Id`, `book`.`Title` AS `Title`, `book`.`Publisher_Name` AS `Publisher_Name`, `book`.`Pub_Year` AS `Pub_Year` FROM `book` WHERE `book`.`Pub_Year` between '2001' and '2004' ;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `book`
---
 ALTER TABLE `book`
   ADD PRIMARY KEY (`Book_Id`),
   ADD KEY `Publisher_Name` (`Publisher_Name`);
 
---
--- Indexes for table `book_authors`
---
+
 ALTER TABLE `book_authors`
   ADD PRIMARY KEY (`Book_Id`);
 
---
--- Indexes for table `book_copies`
---
+
 ALTER TABLE `book_copies`
   ADD PRIMARY KEY (`Book_Id`,`Program_Id`),
   ADD KEY `Program_Id` (`Program_Id`);
 
---
--- Indexes for table `book_lending`
---
+
 ALTER TABLE `book_lending`
   ADD PRIMARY KEY (`Book_Id`,`Program_Id`,`Card_No`),
   ADD KEY `Program_Id` (`Program_Id`);
 
---
--- Indexes for table `library_program`
---
 ALTER TABLE `library_program`
   ADD PRIMARY KEY (`Program_Id`);
 
---
--- Indexes for table `publisher`
---
+
 ALTER TABLE `publisher`
   ADD PRIMARY KEY (`Name`);
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `book`
---
 ALTER TABLE `book`
   ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`Publisher_Name`) REFERENCES `publisher` (`Name`);
 
---
--- Constraints for table `book_authors`
---
+
 ALTER TABLE `book_authors`
   ADD CONSTRAINT `book_authors_ibfk_1` FOREIGN KEY (`Book_Id`) REFERENCES `book` (`Book_Id`) ON DELETE CASCADE;
 
---
--- Constraints for table `book_copies`
---
+
 ALTER TABLE `book_copies`
   ADD CONSTRAINT `book_copies_ibfk_1` FOREIGN KEY (`Book_Id`) REFERENCES `book` (`Book_Id`) ON DELETE CASCADE,
   ADD CONSTRAINT `book_copies_ibfk_2` FOREIGN KEY (`Program_Id`) REFERENCES `library_program` (`Program_Id`) ON DELETE CASCADE;
 
---
--- Constraints for table `book_lending`
---
+
 ALTER TABLE `book_lending`
   ADD CONSTRAINT `book_lending_ibfk_1` FOREIGN KEY (`Book_Id`) REFERENCES `book` (`Book_Id`) ON DELETE CASCADE,
   ADD CONSTRAINT `book_lending_ibfk_2` FOREIGN KEY (`Program_Id`) REFERENCES `library_program` (`Program_Id`) ON DELETE CASCADE;
---
--- Database: `lab2`
---
+
 CREATE DATABASE IF NOT EXISTS `lab2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `lab2`;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `customer`
---
 
 CREATE TABLE `customer` (
   `Customer_id` int(11) NOT NULL,
@@ -334,9 +206,7 @@ CREATE TABLE `customer` (
   `Salesman_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `customer`
---
+
 
 INSERT INTO `customer` (`Customer_id`, `Cust_Name`, `City`, `Grade`, `Salesman_id`) VALUES
 (901, 'Jeevi', 'Maddur', 9, 100),
@@ -347,12 +217,7 @@ INSERT INTO `customer` (`Customer_id`, `Cust_Name`, `City`, `Grade`, `Salesman_i
 (906, 'Niru', 'Bangalore', 8, 300),
 (907, 'Ranu', 'Bangalore', 6, 600);
 
--- --------------------------------------------------------
 
---
--- Stand-in structure for view `highorder`
--- (See below for the actual view)
---
 CREATE TABLE `highorder` (
 `Salesman_id` int(11)
 ,`Customer_id` int(11)
@@ -360,11 +225,6 @@ CREATE TABLE `highorder` (
 ,`Ord_Date` date
 );
 
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
 
 CREATE TABLE `orders` (
   `Ord_No` int(11) NOT NULL,
@@ -374,9 +234,6 @@ CREATE TABLE `orders` (
   `Salesman_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `orders`
---
 
 INSERT INTO `orders` (`Ord_No`, `Purchase_Amt`, `Ord_Date`, `Customer_id`, `Salesman_id`) VALUES
 (201, 100, '2021-11-18', 901, 100),
@@ -385,11 +242,6 @@ INSERT INTO `orders` (`Ord_No`, `Purchase_Amt`, `Ord_Date`, `Customer_id`, `Sale
 (205, 700, '2021-08-18', 905, 500),
 (206, 1000, '2021-11-18', 906, 600);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `salesman`
---
 
 CREATE TABLE `salesman` (
   `Salesman_id` int(11) NOT NULL,
@@ -398,9 +250,7 @@ CREATE TABLE `salesman` (
   `Commission` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `salesman`
---
+
 
 INSERT INTO `salesman` (`Salesman_id`, `Name`, `City`, `Commission`) VALUES
 (100, 'ABC', 'Mandya', 1000),
@@ -410,67 +260,39 @@ INSERT INTO `salesman` (`Salesman_id`, `Name`, `City`, `Commission`) VALUES
 (500, 'PQR', 'Maddur', 5000),
 (600, 'Ani', 'Banglore', 6000);
 
--- --------------------------------------------------------
 
---
--- Structure for view `highorder`
---
 DROP TABLE IF EXISTS `highorder`;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `highorder`  AS SELECT `x`.`Salesman_id` AS `Salesman_id`, `x`.`Customer_id` AS `Customer_id`, `x`.`Purchase_Amt` AS `Purchase_Amt`, `x`.`Ord_Date` AS `Ord_Date` FROM `orders` AS `x` WHERE `x`.`Purchase_Amt` = (select max(`y`.`Purchase_Amt`) from `orders` `y` where `x`.`Ord_Date` = `y`.`Ord_Date`) ;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `customer`
---
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`Customer_id`),
   ADD KEY `Salesman_id` (`Salesman_id`);
 
---
--- Indexes for table `orders`
---
+
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`Ord_No`),
   ADD KEY `Customer_id` (`Customer_id`),
   ADD KEY `Salesman_id` (`Salesman_id`);
 
---
--- Indexes for table `salesman`
---
+
 ALTER TABLE `salesman`
   ADD PRIMARY KEY (`Salesman_id`);
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `customer`
---
+
 ALTER TABLE `customer`
   ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`Salesman_id`) REFERENCES `salesman` (`Salesman_id`) ON DELETE SET NULL;
 
---
--- Constraints for table `orders`
---
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`Customer_id`) REFERENCES `customer` (`Customer_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`Salesman_id`) REFERENCES `salesman` (`Salesman_id`) ON DELETE CASCADE;
---
--- Database: `phpmyadmin`
---
+
 CREATE DATABASE IF NOT EXISTS `phpmyadmin` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 USE `phpmyadmin`;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__bookmark`
---
 
 CREATE TABLE `pma__bookmark` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -480,11 +302,6 @@ CREATE TABLE `pma__bookmark` (
   `query` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Bookmarks';
 
--- --------------------------------------------------------
-
---
--- Table structure for table `pma__central_columns`
---
 
 CREATE TABLE `pma__central_columns` (
   `db_name` varchar(64) NOT NULL,
@@ -497,11 +314,6 @@ CREATE TABLE `pma__central_columns` (
   `col_default` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Central list of columns';
 
--- --------------------------------------------------------
-
---
--- Table structure for table `pma__column_info`
---
 
 CREATE TABLE `pma__column_info` (
   `id` int(5) UNSIGNED NOT NULL,
@@ -516,22 +328,14 @@ CREATE TABLE `pma__column_info` (
   `input_transformation_options` varchar(255) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Column information for phpMyAdmin';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__designer_settings`
---
 
 CREATE TABLE `pma__designer_settings` (
   `username` varchar(64) NOT NULL,
   `settings_data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Settings related to Designer';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__export_templates`
---
 
 CREATE TABLE `pma__export_templates` (
   `id` int(5) UNSIGNED NOT NULL,
@@ -541,22 +345,13 @@ CREATE TABLE `pma__export_templates` (
   `template_data` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Saved export templates';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__favorite`
---
 
 CREATE TABLE `pma__favorite` (
   `username` varchar(64) NOT NULL,
   `tables` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Favorite tables';
 
--- --------------------------------------------------------
-
---
--- Table structure for table `pma__history`
---
 
 CREATE TABLE `pma__history` (
   `id` bigint(20) UNSIGNED NOT NULL,
@@ -567,11 +362,6 @@ CREATE TABLE `pma__history` (
   `sqlquery` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='SQL history for phpMyAdmin';
 
--- --------------------------------------------------------
-
---
--- Table structure for table `pma__navigationhiding`
---
 
 CREATE TABLE `pma__navigationhiding` (
   `username` varchar(64) NOT NULL,
@@ -581,11 +371,7 @@ CREATE TABLE `pma__navigationhiding` (
   `table_name` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Hidden items of navigation tree';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__pdf_pages`
---
 
 CREATE TABLE `pma__pdf_pages` (
   `db_name` varchar(64) NOT NULL DEFAULT '',
@@ -593,22 +379,14 @@ CREATE TABLE `pma__pdf_pages` (
   `page_descr` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='PDF relation pages for phpMyAdmin';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__recent`
---
 
 CREATE TABLE `pma__recent` (
   `username` varchar(64) NOT NULL,
   `tables` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Recently accessed tables';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__relation`
---
 
 CREATE TABLE `pma__relation` (
   `master_db` varchar(64) NOT NULL DEFAULT '',
@@ -619,11 +397,7 @@ CREATE TABLE `pma__relation` (
   `foreign_field` varchar(64) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='Relation table';
 
--- --------------------------------------------------------
 
---
--- Table structure for table `pma__savedsearches`
---
 
 CREATE TABLE `pma__savedsearches` (
   `id` int(5) UNSIGNED NOT NULL,
